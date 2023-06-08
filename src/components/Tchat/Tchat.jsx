c9b810d54dff3393bfd05d533f903d74197c03ff
Antoine Elissagaray <antoine.elissagaray@orange.fr>
2023-06-08 14:04:59 +0200

    commit branch

---
+++ src/components/Tchat/Tchat.jsx
@@ -0,0 +1,38 @@
+
+
+function Tchat() {
+
+
+    return (
+
+        <section className="main-tchat">
+
+            <section className="container-matchs">
+                <div className="wrap-matchs"></div>
+            </section>
+
+            <section className="container-tchat">
+                <div className="tchat-history-match"></div>
+                <div className="tchat-messages"></div>
+                <div className="tchat-input"></div>
+            </section>
+            <section className="container-description">
+                <div className="description-match"></div>
+                <div className="buttons-match"></div>
+
+            </section>
+
+
+
+
+
+
+        </section>
+
+
+
+    )
+
+
+
+}
