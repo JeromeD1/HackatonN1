f486d2964fa83573ce3754e4ea08a1876138c120
Jerome Desouches <jerome.desouches@gmail.com>
2023-06-08 08:57:59 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./screens/Home";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/characters" element={<Characters />} /> */}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default Header;
