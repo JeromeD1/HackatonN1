3d887e21eec4fb188181a7618d793e704ab03f18
Jerome Desouches <jerome.desouches@gmail.com>
2023-06-08 11:02:13 +0200

    ajout fetch api star wars dans home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,4 +1,16 @@
+import {useState, useEffect} from 'react'
+
 const Home = () => {
+
+    const [peoples, setPeoples] = useState([]);
+  
+    useEffect(() => {
+      fetch("https://miadil.github.io/starwars-api/api/all.json")
+        .then((res) => res.json())
+        .then((res) => console.log(res) || setPeoples(res));
+    }, []);
+
+
   return <>je suis dans la page home</>;
 };
 
